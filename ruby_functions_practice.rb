b7842a1b6ea42378eeb054f89952caedb68ae3a4
JohnHG1 <honisz@yahoo.com>
2020-01-14 16:42:53 +0000

    Initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,102 @@
+def return_10()
+  return 10
+end
+
+def add(number1, number2)
+  return number1 + number2
+end
+
+def subtract(number1, number2)
+  return number1 - number2
+end
+
+def multiply(number1, number2)
+  return number1 * number2
+end
+
+def divide(number1, number2)
+  return number1 / number2
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string1, string2)
+  return string1 + string2
+end
+
+def add_string_as_number(string1, string2)
+  return string1.to_i() + string2.to_i()
+end
+
+def number_to_full_month_name(num)
+  case num
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  else
+    return "Error"
+  end
+end
+
+def number_to_short_month_name(num)
+  case num
+  when 1
+    return "Jan"
+  when 2
+    return "Feb"
+  when 3
+    return "Mar"
+  when 4
+    return "Apr"
+  when 5
+    return "May"
+  when 6
+    return "Jun"
+  when 7
+    return "Jul"
+  when 8
+    return "Aug"
+  when 9
+    return "Sep"
+  when 10
+    return "Oct"
+  when 11
+    return "Nov"
+  when 12
+    return "Dec"
+  else
+    return "Error"
+  end
+end
+
+def volume_of_cube(num)
+  return num**3
+end
+
+def radius_of_a_sphere(num)
+
+
+end
